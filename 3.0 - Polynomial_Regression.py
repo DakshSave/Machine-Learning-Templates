9f64737a40f7fc243adc96e25caef590cb3e8b5a
DakshSave <dakshsave9@gmail.com>
2025-05-24 11:45:37 +0530

    Update and rename 3.0 - Polynomial Regression.py to 3.0 - Polynomial_Regression.py

---
+++ 3.0 - Polynomial_Regression.py
@@ -0,0 +1,7 @@
+#Import all the necessary libraries.
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+#
