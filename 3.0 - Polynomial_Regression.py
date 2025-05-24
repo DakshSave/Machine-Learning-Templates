0c55bd2e3fad8807d2148b92ca4cf217279848c0
DakshSave <dakshsave9@gmail.com>
2025-05-24 12:05:51 +0530

    Update 3.0 - Polynomial_Regression.py

--- 3.0 - Polynomial_Regression.py
+++ 3.0 - Polynomial_Regression.py
@@ -18,3 +18,14 @@ Y = df["_"] # Replace the underscore with the header of what would be the depend
 #Transform the data into a polynomial.
 poly = PolynomialFeatures(degree = _) #Replace the underscore with what you want to be the degree of the polynomial.
 X_poly = poly.fit_transform(X)
+
+#Create a linear regression model.
+pr_model = LinearRegression()
+
+#Fit the model with the data.
+pr_model.fit(X_poly, Y)
+
+#Obtain the prediction.
+Y_prediction = pr_model.predict(X_poly)
+
+#Plot the actual and predicted data.
