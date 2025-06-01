b52fc560e39080eabc703bfdb09a1549aed84003
DakshSave <dakshsave9@gmail.com>
2025-06-01 16:13:32 +0530

    Update 4.0 - Exponential_Regression.py

--- 4.0 - Exponential_Regression.py
+++ 4.0 - Exponential_Regression.py
@@ -1,1 +1,36 @@
+#Import the necessary libraries.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
+
+data = "_"
+
+
+df = pd.read_csv(data)
+
+
+X = df["_"]
+Y = df["_"]
+
+
+def exponential_func(x, a, b):
+    return a*np.exp(b*x)
+
+
+params, _ = curve_fit(exponential_func, X, Y)
+a, b = params
+
+
+Y_prediction = exponential_func(X, a, b)
+
+#Plot the data.
+plt.scatter(X, Y, color="blue", label="Actual Data")
+plt.plot(X, Y_prediction, color="red", label="Predicted Data")
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
+plt.title("Exponential Regression")
+plt.legend()
+plt.grid(True)
+plt.show()
