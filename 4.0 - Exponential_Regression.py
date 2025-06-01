87452757ba432f1f0138177eadb763d21cbdc2b1
DakshSave <dakshsave9@gmail.com>
2025-06-01 16:23:23 +0530

    Update 4.0 - Exponential_Regression.py

--- 4.0 - Exponential_Regression.py
+++ 4.0 - Exponential_Regression.py
@@ -4,33 +4,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-
-data = "_"
-
-
+#Prepare the data.
+data = "_" #Replace the underscore with the path to the file.
 df = pd.read_csv(data)
 
+#Clean the data.
+df.dropna()
 
-X = df["_"]
-Y = df["_"]
+#Assign the variables.
+X = df["_"] #Replace the underscore with the header of the independent variable.
+Y = df["_"] #Replace the underscore with the header of the dependent variable.
 
-
+#Create the exponential function.
 def exponential_func(x, a, b):
     return a*np.exp(b*x)
 
-
+#Fit the function and obtain the parameters.
 params, _ = curve_fit(exponential_func, X, Y)
 a, b = params
 
-
+#Obtain the predictions.
 Y_prediction = exponential_func(X, a, b)
 
-#Plot the data.
+#Plot the actual data and the prediction data.
 plt.scatter(X, Y, color="blue", label="Actual Data")
 plt.plot(X, Y_prediction, color="red", label="Predicted Data")
-plt.xlabel("Feature 1")
-plt.ylabel("Feature 2")
+plt.xlabel("X")
+plt.ylabel("Y / Y_prediction")
 plt.title("Exponential Regression")
 plt.legend()
 plt.grid(True)
 plt.show()
+
+#IMPORTANT - This code will show error until the underscores are filled.
