7970b3acedf7c4ed65d91b6411055ee757c6de58
DakshSave <dakshsave9@gmail.com>
2025-06-03 20:31:30 +0530

    Update 3.0 - Polynomial_Regression.py

--- 3.0 - Polynomial_Regression.py
+++ 3.0 - Polynomial_Regression.py
@@ -1,41 +1,44 @@
 #Import all the necessary libraries.
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.metrics import accuracy_score
 
 #Prepare the data.
 data = "_" #Replace the underscore with the file path.
-df = pd.read_csv(data) #Change the csv to any other format respective to your file.
+df = pd.read_csv(data) #You can change csv to excel, json or hdf according to your data file.
 
 #Clean the data.
-df.dropna()
+df.dropna(inplace = True)
 
 #Assign the variables.
 X = df[["_"]] #Replace the underscore with the header(s) of what would be the independent variable(s).
 Y = df["_"] # Replace the underscore with the header of what would be the dependent variable.
 
+#Split the data into training and testing sets.
+X_train, X_test, Y_train, Y_test = (X, Y, test_size = _, random_state = 42)
+
+#Scale the data.
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
 #Transform the data into a polynomial.
 poly = PolynomialFeatures(degree = _) #Replace the underscore with what you want to be the degree of the polynomial.
-X_poly = poly.fit_transform(X)
+X_train = poly.fit_transform(X_train)
+X_test = poly.transform(X_test)
 
 #Create a linear regression model.
-pr_model = LinearRegression()
+model = LinearRegression()
 
 #Fit the model with the data.
-pr_model.fit(X_poly, Y)
+model.fit(X_train, Y_train)
 
 #Obtain the prediction.
-Y_prediction = pr_model.predict(X_poly)
+Y_prediction = model.predict(X_test)
 
-#Plot the actual and predicted data.
-plt.scatter(X, Y, color = "blue", label = "Actual Data")
-plt.plot(X, Y_prediction, color = "red", label = "Predicted Data")
-plt.xlabel("X")
-plt.ylabel("Y / Y_prediction")
-plt.title("Polynomial Regression")
-plt.legend()
-plt.grid(True)
-plt.show
+
 
 #IMPORTANT - This code will show error until the underscores are filled.
