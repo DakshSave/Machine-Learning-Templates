d2542102174bf31dbe02ee17c193c9ff403ddc88
DakshSave <dakshsave9@gmail.com>
2025-06-03 20:23:01 +0530

    Update 2.0 - MLR_Estimator.py

--- 2.0 - MLR_Estimator.py
+++ 2.0 - MLR_Estimator.py
@@ -1,1 +1,50 @@
+#Import all the necessary libraries.
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 
+#Prepare the data.
+data = "_" #Replace the underscore with the path to your desired file.
+df = pd.read_csv(data) #You can use excel, json or hdf also according to your data.
+
+#Clean the data.
+df = df.dropna()
+
+#Assign the variables.
+X = df[["_", "_"]] #Replace the underscore with the header(s) of the column(s) of the independent variable(s).
+Y = df["_"] #Replace the underscore with the header of the column of the dependent/target variable.
+
+#Split the data into training and testing sets.
+X_train, X_test, Y_train, Y_test = (X, Y, test_size = _, random_state = 42) #The value of test_size (where the underscore is) must be between 1 and 0 (representing percentages). 
+
+#Scale the data.
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+#Create the linear regression model.
+model = LinearRegression()
+
+#Fit the model with the data.
+model.fit(X_train, Y_train)
+
+#Get the prediction.
+Y_prediction = model.predict(X_test)
+
+#Get the slope and the intercept.
+slope = model.coef_
+intercept = model.intercept_
+
+#Get the accuracy score.
+accuracy = accuracy_score(Y_test, Y_prediction)
+
+#Print the slope, intercept, prediction and the accuracy score.
+print("Slope =", slope)
+print("Intercept =", intercept)
+print("Predictions :\n", Y_prediction)
+print("Accuracy Score =", accuracy)
+
+
+#IMPORTANT - This code will show error until the underscores are filled.
