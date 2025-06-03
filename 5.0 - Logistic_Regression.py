1802425dec85a58072a339b76529fbcbe23cea64
DakshSave <dakshsave9@gmail.com>
2025-06-03 22:37:09 +0530

    Update 5.0 - Logistic_Regression.py

--- 5.0 - Logistic_Regression.py
+++ 5.0 - Logistic_Regression.py
@@ -15,7 +15,7 @@ X = df[["_"]].values #Replace the underscore with the header of the independent 
 Y = df["_"] #Replace the underscore with the header of the dependent variable.
 
 #Split the data into training and testing sets.
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = _, random_state = 42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = _, random_state = 42) #The value of test size must be between 0 and 1 (which represent precentages).
 
 #Scale the data.
 scaler = StandardScaler()
