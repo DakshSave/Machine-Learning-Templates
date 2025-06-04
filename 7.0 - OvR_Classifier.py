46c333b2e819cca24a32cf60b1ff2933aadc7ca9
DakshSave <dakshsave9@gmail.com>
2025-06-04 22:33:36 +0530

    Create 7.0 - OvR_Classifier.py

---
+++ 7.0 - OvR_Classifier.py
@@ -0,0 +1,7 @@
+#Import all necessary libraries.
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import accuracy_score
