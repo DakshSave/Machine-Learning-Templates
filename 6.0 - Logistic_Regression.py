8393cff6f61fd3324f2ac80db04b25c35a22936a
DakshSave <dakshsave9@gmail.com>
2025-08-04 20:11:08 +0530

    Update 6.0 - Logistic_Regression.py

--- 6.0 - Logistic_Regression.py
+++ 6.0 - Logistic_Regression.py
@@ -1,7 +1,8 @@
 #Import the necessary libraries.
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler, Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
