8ebf67fcd8a505c8aa8cbee6170a862c2a8cc32c
DakshSave <dakshsave9@gmail.com>
2025-08-04 19:42:56 +0530

    Update Ridge_Regression.py

--- Ridge_Regression.py
+++ Ridge_Regression.py
@@ -1,1 +1,48 @@
+#Import all the necessary libraries.
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, Pipeline
+from sklearn.metrics import mean_squared_error
 
+#Prepare the data.
+data = "_" #Replace the underscore with the path to your desired file.
+df = pd.read_csv(data) #You can use excel, json or hdf also according to your data.
+
+#Clean the data.
+df.dropna(inplace = True)
+
+#Assign the variables.
+X = df[["_"]] #Replace the underscore with the header of the column of the independent variable.
+Y = df["_"] #Replace the underscore with the header of the column of the dependent/target variable.
+
+#Split the data into training and testing sets.
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = _, random_state = 42) #The value of test_size (where the underscore is) must be between 0 and 1 (representing percentages). 
+
+#Create a pipeline.
+pipeline = Pipeline([("scaler", StandardScaler()), ("model", Ridge(alpha = _))]) #Replace the underscore with the value of the regularization term (alpha).
+
+#Fit the pipeline with the data.
+pipeline.fit(X_train, Y_train)
+
+#Make separate variables of each step for better readability (optional)
+scaler = pipeline.named_steps["scaler"]
+model = pipeline.named_steps["model"]
+
+#Get the prediction.
+Y_prediction = pipeline.predict(X_test)
+
+#Get the slope and the intercept.
+slope = model.coef_
+intercept = model.intercept_
+
+#Get the accuracy score.
+mse = mean_squared_error(Y_test, Y_prediction)
+
+#Print the slope, intercept, prediction and the accuracy score.
+print("Slope =", slope)
+print("Intercept =", intercept)
+print("Predictions :\n", Y_prediction)
+print("Mean Squared Error =", mse)
+
+#IMPORTANT - This code will show error until the underscores are filled.
