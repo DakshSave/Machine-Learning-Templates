f8ccd1f9d44c3860c4b30a5d51667ce93877fcff
DakshSave <dakshsave9@gmail.com>
2025-08-04 19:54:42 +0530

    Update 1.0 - SLR_Estimator.py

--- 1.0 - SLR_Estimator.py
+++ 1.0 - SLR_Estimator.py
@@ -17,7 +17,7 @@ X = df[["_"]] #Replace the underscore with the header of the column of the indep
 Y = df["_"] #Replace the underscore with the header of the column of the dependent/target variable.
 
 #Split the data into training and testing sets.
-X_train, X_test, Y_train, Y_test = (X, Y, test_size = _, random_state = 42) #The value of test_size (where the underscore is) must be between 0 and 1 (representing percentages). 
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = _, random_state = 42) #The value of test_size (where the underscore is) must be between 0 and 1 (representing percentages). 
 
 #Create a pipeline.
 pipeline = Pipeline([("scaler", StandardScaler()), ("model", LinearRegression())])
