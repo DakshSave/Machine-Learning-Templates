15296747b973fe16c79bf0a2a593ca571e09680f
DakshSave <dakshsave9@gmail.com>
2025-08-04 20:06:22 +0530

    Update 8.0 - Decision_Tree.py

--- 8.0 - Decision_Tree.py
+++ 8.0 - Decision_Tree.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 
 #Prepare the data.
@@ -20,6 +21,7 @@ Y = ["_"] #Replace the underscore with the header of the independent variable.
 #Split the data into training and testing sets.
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = _, random_state = 42) #The value of test size must be between 0 and 1 (representating percentages).
 
+#Create a pipeline.
 pipeline = Pipeline([("scaler", StandardScaler()), ("model", DecisionTreeClassifier(max_depth = _, min_samples_split = _, min_samples_split = _, min_samples_leaf = _, max_leaf_nodes = _, random_state = 42))])
 '''
 IMPORTANT
@@ -31,14 +33,15 @@ max_leaf_nodes = Maximum number of leaf nodes.
 max_features = Maximum number of features considered for a split.
 '''
 
+#Make separate variables of each step for better readability (optional).
 scaler = pipeline.named_steps["scaler"]
 model = pipeline.named_steps["model"]
 
-#Fit the model with the training data.
-model.fit(X_train, Y_train)
+#Fit the pipeline with the training data.
+pipeline.fit(X_train, Y_train)
 
 #Obtain the predictions.
-Y_prediction = model.predict(X_test)
+Y_prediction = pipeline.predict(X_test)
 
 #Get the accuracy score.
 accuracy = accuracy_score(Y_test, Y_prediction)
