db1eaa3eaeda7d25074d6abf15fcb8a52c1efb72
DakshSave <dakshsave9@gmail.com>
2025-09-02 18:17:45 +0530

    Update 12 - Regression_Tree.py

--- 12 - Regression_Tree.py
+++ 12 - Regression_Tree.py
@@ -15,8 +15,8 @@ df = pd.read_csv(data) #You can replace the csv with excel, json or hdf respecti
 df.dropna(inplace = True)
 
 #Assign the variables.
-X = [["_"]] #Replace the underscore(s) with the header(s) of the independent variable(s).
-Y = ["_"] #Replace the underscore with the header of the independent variable.
+X = df[["_"]] #Replace the underscore(s) with the header(s) of the independent variable(s).
+Y = df["_"] #Replace the underscore with the header of the independent variable.
 
 #Split the data into training and testing sets.
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = _, random_state = 42) #The value of test size must be between 0 and 1 (representating percentages).
