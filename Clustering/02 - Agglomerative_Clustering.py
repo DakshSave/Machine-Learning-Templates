d0917f7bbc8048ffaa5a6758b1ae2b4992c2cc8b
DakshSave <dakshsave9@gmail.com>
2025-09-07 19:14:53 +0530

    Update 02 - Agglomerative_Clustering.py

--- Clustering/02 - Agglomerative_Clustering.py
+++ Clustering/02 - Agglomerative_Clustering.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
 
 #Prepare the data.
 data = "_" #Replace the underscore with the path to your file.
@@ -12,24 +11,23 @@ df = pd.read_csv(data) #You can replace the csv with excel, json or hdf respecti
 #Clean the data.
 df.dropna(inplace = True)
 
-#Assign the variables (independent variables only).
+#Assign the variables.
 X = df[["_"]] #Replace the underscore(s) with header(s) of your independent variable(s).
 
-#Create a pipeline.
-pipeline = Pipeline([("scaler", StandardScaler()), ("model", AgglomerativeClustering(n_clusters = _, linkage = "_"))]) #Replace the underscore with number of clusters. Linkage options: "ward", "complete", "average", "single".
+#Scale the data.
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
 
-#Create separate variables of each step in the pipeline for better readability.
-scaler = pipeline.named_steps["scaler"]
-model = pipeline.named_steps["model"]
+#Create the model.
+model = AgglomerativeClustering(n_clusters = _, linkage = "_") #Replace the underscore with number of clusters. Linkage options: "ward", "complete", "average", "single".
 
-#Fit the model with data.
-X_scaled = scaler.fit_transform(X)   # AgglomerativeClustering does not support pipeline .fit(X)
+#Fit the model with data and obtain clusters.
 clusters = model.fit_predict(X_scaled)
 
 #Add cluster labels to the dataframe.
 df["Cluster"] = clusters
 
-#Print the assigned clusters.
+#Print the cluster assignments.
 print("Cluster assignments :\n", clusters)
 
 #IMPORTANT - This code will show error until the underscores are filled.
