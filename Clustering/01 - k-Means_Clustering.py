c4a691d7d36ad0434c5fd010d8cc414ab435c951
DakshSave <dakshsave9@gmail.com>
2025-09-07 19:13:39 +0530

    Update 01 - k-Means_Clustering.py

--- Clustering/01 - k-Means_Clustering.py
+++ Clustering/01 - k-Means_Clustering.py
@@ -1,10 +1,8 @@
 #Import all necessary libraries.
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
 
 #Prepare the data.
 data = "_" #Replace the underscore with the path to your file.
@@ -13,26 +11,23 @@ df = pd.read_csv(data) #You can replace the csv with excel, json or hdf respecti
 #Clean the data.
 df.dropna(inplace = True)
 
-#Assign the variables (only independent variables, since clustering is unsupervised).
+#Assign the variables.
 X = df[["_"]] #Replace the underscore(s) with header(s) of your independent variable(s).
 
-#Create a pipeline.
-pipeline = Pipeline([("scaler", StandardScaler()), ("model", KMeans(n_clusters = _, random_state = 42))]) #Replace the underscore with number of clusters.
+#Scale the data.
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
 
-#Create separate variables of each step in the pipeline for better readability.
-scaler = pipeline.named_steps["scaler"]
-model = pipeline.named_steps["model"]
+#Create the model.
+model = KMeans(n_clusters = _, random_state = 42) #Replace the underscore with number of clusters.
 
-#Fit the model with data.
-pipeline.fit(X)
-
-#Obtain the cluster predictions.
-clusters = pipeline.predict(X)
+#Fit the model with data and obtain clusters.
+clusters = model.fit_predict(X_scaled)
 
 #Add cluster labels to the dataframe.
 df["Cluster"] = clusters
 
-#Print the assigned clusters.
+#Print the cluster assignments.
 print("Cluster assignments :\n", clusters)
 
 #IMPORTANT - This code will show error until the underscores are filled.
